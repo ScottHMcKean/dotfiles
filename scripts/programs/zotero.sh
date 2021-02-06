237b1884719bb696351aa4de0e4a88c55b641d65
Scott McKean <scott.mckean@ucalgary.ca>
2021-02-06 15:00:18 -0700

    update zotero with better bibtex

--- scripts/programs/zotero.sh
+++ scripts/programs/zotero.sh
@@ -9,3 +9,8 @@ sudo apt install zotero
 # requires rclone & gdrive to be setup
 # copies cloud references to Zotero default
 rclone copy gdrive:zotero ~/Zotero/
+
+# get better bibtex
+# https://retorque.re/zotero-better-bibtex/installation/
+cd ~/Zotero/
+wget https://github.com/retorquere/zotero-better-bibtex/releases/download/v5.2.112/zotero-better-bibtex-5.2.112.xpi
