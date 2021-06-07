b7403574ac0e033e2bee4448d6aa23ae67781baf
ScottHMcKean <scott.mckean@ucalgary.ca>
2021-06-06 21:59:32 -0600

    add preferred reduction

--- scripts/pdf_tools.sh
+++ scripts/pdf_tools.sh
@@ -3,5 +3,5 @@
 # reduce a pdf using ghostscript
 # setting: /screen, /ebook, /printer, /prepress
 gs -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=setting -sOutputFile=output.pdf input.pdf
-# e.g.
-gs -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/ebook -sOutputFile=output.pdf input.pdf
+# e.g. for papers
+gs -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/printer -sOutputFile=output.pdf input.pdf
